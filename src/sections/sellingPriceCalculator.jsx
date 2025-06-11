08df5225476fcbad22a630205e2f429e79f06bd0
Analyce Ferreira <anallyce_@live.com>
2025-06-10 21:43:33 -0400

    MIGRATE: backup to new device

---
+++ src/sections/sellingPriceCalculator.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+function SellingPriceCalculator() {
+  return (
+    <div className="selling w-full h-full">
+      <div className="w-full">
+        <div>Header</div>
+        <div>
+          Content
+          <div>Campos</div>
+          <div>Result</div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export { SellingPriceCalculator };
